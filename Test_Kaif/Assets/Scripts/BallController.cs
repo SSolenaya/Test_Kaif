787861226b0b0f0173bd009240d6c4bfc3f4e672
SSolenaya <saltssv@gmail.com>
2023-03-03 10:12:55 +0200

    coolDown for ball

--- Test_Kaif/Assets/Scripts/BallController.cs
+++ Test_Kaif/Assets/Scripts/BallController.cs
@@ -10,7 +10,10 @@ public class BallController : MonoBehaviour
     [SerializeField] private RaycastController _raycastController;
     [SerializeField] private Ball _ball;
     [SerializeField] private Transform _spawnPoint;         //  ball spawning point
-    [SerializeField] private float _radiusOfBlast = 60f;    //  TODO: move to SO or Config
+    private float _radiusOfBlast = 60f;             //  TODO: move to SO or Config
+    private float _coolDownForNewBall = 0.5f;       //  TODO: move to SO or Config
+    private bool _isBallReady = true;
+    private CountdownTimer _coolDownTimer = new CountdownTimer();
 
     void Start()
     {
@@ -19,13 +22,16 @@ public class BallController : MonoBehaviour
 
     private void ShootBall(RaycastHit raycastHit)
     {
+        if (!_isBallReady) return;
         var ball = Instantiate(_ball, _spawnPoint);
         Vector3 endPos = raycastHit.point;
         ball.SetDestination(_spawnPoint.position, endPos, () => CubeController.Inst.CheckDestroyingCubes(endPos, _radiusOfBlast));
+        _isBallReady = false;
+        _coolDownTimer.Setup(_coolDownForNewBall, () => _isBallReady = true); 
     }
 
     void Update()
     {
-        
+        _coolDownTimer.Countdown();
     }
 }
